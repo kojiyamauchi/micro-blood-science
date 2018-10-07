5a0d4c718b6ca6d92ece50080d8b0de270dfce02
kojiyamauchi <all.the.small.things@me.com>
2018-10-08 08:17:48 +0900

    update AnchorLink.ts (temporary)

--- base/modules/scroll/AnchorLink.ts
+++ base/modules/scroll/AnchorLink.ts
@@ -7,21 +7,20 @@ import jump from 'jump.js'
 
 // Anchor Link Scroll Functions.
 export default class AnchorLink {
+  // Types.
   target: NodeListOf<HTMLElement>
-  targetLen: number
 
   constructor() {
     this.target = document.querySelectorAll('.fn-anchor')
-    this.targetLen = this.target.length
   }
 
-  coreFunc() {
-    for (let i = 0; i < this.targetLen; i++) {
-      this.target[i].onclick = (event: MouseEvent) => {
-        const getAnchor = this.target[i].dataset.anchor
+  callFunc() {
+    Array.from(this.target).map(info => {
+      info.onclick = (event: MouseEvent) => {
+        const getAnchor = info.dataset.anchor
         jump(`.${getAnchor}`)
         event.preventDefault()
       }
-    }
+    })
   }
 }
